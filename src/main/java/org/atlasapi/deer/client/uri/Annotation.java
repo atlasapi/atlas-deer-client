756c8df46c2bae90a19cded598848fdecb2306ff
Tristan Beardmore-Gray <tristan.beardmore-gray@hotmail.co.uk>
2020-01-15 16:18:45 +0000

    Added awards annotation

--- src/main/java/org/atlasapi/deer/client/uri/Annotation.java
+++ src/main/java/org/atlasapi/deer/client/uri/Annotation.java
@@ -36,7 +36,8 @@ public enum Annotation {
     ALL_MERGED_BROADCASTS,
     ALL_BROADCASTS,
     CHANNEL_GROUP_INFO,
-    REGIONS
+    REGIONS,
+    AWARDS,
     ;
 
     private static final BiMap<String, Annotation> LOOKUP =
